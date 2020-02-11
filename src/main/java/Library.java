811f14cfd083a495dbe694e130a057b6446292aa
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-02-11 16:45:36 +0000

    MVP complete, added conditional to check capacity of library before adding a book

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,12 +2,24 @@ import java.util.ArrayList;
 
 public class Library {
     private ArrayList<Book> books;
+    private Integer capacity;
 
     public Library(){
-        books = new ArrayList<Book>();
+        this.books = new ArrayList<Book>();
+        this.capacity = 5;
     }
 
     public int getBooksLength() {
         return this.books.size();
+    }
+
+    public void addBook(Book book) {
+        if (this.getBooksLength() < this.capacity){
+            this.books.add(book);
+        }
+    }
+
+    public int getCapacity() {
+        return this.capacity;
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -5,15 +5,33 @@ import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
     private Library library;
+    private Book book;
 
     @Before
     public void before(){
         library = new Library();
+        book = new Book("Alice In Wonderland", "Lewis Carrol", "fantasy");
     }
 
     @Test
     public void check_books_start_empty(){
         assertEquals(0,library.getBooksLength());
+    }
+
+    @Test
+    public void check_book_can_be_added_to_library(){
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        assertEquals(5,library.getBooksLength());
+    }
+
+    @Test
+    public void can_get_capacity(){
+        assertEquals(5,library.getCapacity());
     }
 
 }
