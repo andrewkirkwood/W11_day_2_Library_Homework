3caadfb5c126afda1aa135d802a323ccf0b722cc
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-02-11 16:33:17 +0000

    book class populated and basic get tests passing for title, author, genre

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+    private Book book;
+
+    @Before
+    public void before(){
+        book = new Book("Alice In Wonderland", "Lewis Carrol", "fantasy");
+    }
+
+    @Test
+    public void check_book_has_title(){
+        assertEquals("Alice In Wonderland", book.getTitle());
+    }
+
+    @Test
+    public void check_book_has_author(){
+        assertEquals("Lewis Carrol", book.getAuthor());
+    }
+
+    @Test
+    public void  check_book_has_genre(){
+        assertEquals("fantasy", book.getGenre());
+    }
+
 }
